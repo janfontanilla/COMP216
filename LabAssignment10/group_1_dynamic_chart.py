10f91a051a1ca9e9cd684b596e5f5dfd964d7943
Jan Fontanilla <janfontanilla12@gmail.com>
2026-04-02 12:27:50 -0400

    Part A section 1: implement update_data thread method

--- LabAssignment10/group_1_dynamic_chart.py
+++ LabAssignment10/group_1_dynamic_chart.py
@@ -1,2 +1,26 @@
 import tkinter as tk
 import threading
+import random
+import time
+
+
+class DynamicChart:
+    
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Dynamic Time Series Chart")
+        self.root.geometry("600x400")
+        
+        # create a values list with random values
+        self.values = [random.randint(10, 100) for _ in range(20)]  #initialize data list
+    
+    # part A: update data in a separate thread
+    def update_data(self):
+        while True:
+            self.values.pop(0)  #remove the first item in the list
+            self.values.append(random.randint(10, 100))  #add a new random value to the end of the list
+            self.display_chart()  #display the list on the canvas
+            time.sleep(0.5)  #sleep for 0.5 seconds
+    
+    def display_chart(self):
+        pass
