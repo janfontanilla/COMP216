0eba2553dad7aee5847e086697b4d53ddc7ed301
adriandot1 <adriandot1@users.noreply.github.com>
2026-04-16 15:06:20 -0400

    Update publisher.py

--- LabAssignment11/publisher.py
+++ LabAssignment11/publisher.py
@@ -5,12 +5,14 @@ import time
 
 util = Util()
 client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
-client.connect("broker.emqx.io", 1883, 60)
+client.connect("broker.emqx.io", 1883, 60) #connects to host, on port 1883. kept alive for 60 sec
 
 for i in range(5):
 
     payload = util.create_data()
+    #converts python into json
     message = json.dumps(payload)
+    #sends message to anyone subscribed 
     client.publish("group3/vitals", message)
     print("Published: ", message)
     time.sleep(2)
